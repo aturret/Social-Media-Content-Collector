24d78960cc17c86b72fda4b3f853fb088d10d230
aturret <enturreopy@gmail.com>
2023-04-23 14:54:06 -0400

    rollback to stable version of threadings

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
-from app import create_app
+from app import create_app, atelebot, threading
 import os
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', '1045')))
+    telebot_thread = threading.Thread(target=atelebot.bot.polling(), daemon=True)
+    telebot_thread.start()
