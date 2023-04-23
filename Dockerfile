14d0987074830e0dc4d2463e061ad05b919cd9d3
aturret <enturreopy@gmail.com>
2023-04-23 15:27:49 -0400

    rollback to stable version of threadings

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,11 @@ COPY ./ /app
 WORKDIR /app
 # EXPOSE 1045
 RUN #curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py" \
-#    && python get-pip.py \
-#    && pip install pyTelegramBotAPI \
-#    && pip install python-telegram-bot \
-#    &&  \
     pip install pipenv \
     && pipenv install
     # && pipenv run gunicorn -w 4 -b 0.0.0.0:1045 wsgi:app
 # CMD 
-ENTRYPOINT pipenv run gunicorn -w 4 -b 0.0.0.0:$PORT wsgi:app
+ENTRYPOINT pipenv run gunicorn -w 1 -b 0.0.0.0:$PORT wsgi:app
 # ENTRYPOINT ["/app/run.sh"]
 
 # docker build -t aturret/socialmediacollector .
--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from waitress import serve
 import app
 
 if __name__ == '__main__':
-    bot_process = app.atelebot.bot.polling(none_stop=True, timeout=90)
-    # bot_process = app.bot_start.start_bot_process()  # Start the Telegram bot process
-    # monitor_thread = app.threading.Thread(target=app.bot_start.monitor_bot_process, args=(bot_process,))
-    # monitor_thread.start()
+    # bot_process = app.atelebot.bot.polling(none_stop=True, timeout=90)
+    bot_process = app.bot_start.start_bot_process()  # Start the Telegram bot process
+    monitor_thread = app.threading.Thread(target=app.bot_start.monitor_bot_process, args=(bot_process,))
+    monitor_thread.start()
     # Run the Flask app with Waitress
     serve(app.create_app(), host='127.0.0.1', port=app.settings.env_var.get('PORT', '1045'))
