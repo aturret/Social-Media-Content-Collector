221e441f388f586f60b00132628fb714ec15620e
aturret <enturreopy@gmail.com>
2023-06-01 21:49:20 -0400

    fix

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# pipenv run gunicorn -w 4 -b 0.0.0.0:1045 wsgi:app
-pipenv run waitress-serve --listen=*:1045 --threads=5 wsgi:app
-#web: pipenv run gunicorn -w 1 -b 0.0.0.0:$PORT --timeout 600 --threads 1 --log-level=debug wsgi:app
-#web: pipenv run waitress-serve --listen=*:$PORT wsgi:app
-#worker: pipenv run python ./bot_run.py
-#web: pipenv run python ./run_multiprocessing.py
