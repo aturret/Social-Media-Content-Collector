9491d2abd8ce7078b8edc725e592ef62f22b1a34
aturret <enturreopy@gmail.com>
2023-07-12 22:28:17 -0400

    video download bugfix

--- app/atelebot.py
+++ app/atelebot.py
@@ -131,6 +131,9 @@ def callback_query(call):
         target_data = formatted_data.pop(query_data[2])
         target_function_kwargs = target_data['extra_kwargs']
         target_function_kwargs['channel'] = True
+        if target_data['target_item_type'] == 'video':
+            target_function_kwargs['download'] = True
+            target_function_kwargs['yt_downloader'] = True
         response_data = target_data['target_function'](target_data['url'], **target_function_kwargs)
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                               text='处理完毕，正在把消息发送到频道……\nProcessing complete, sending message to channel...')
